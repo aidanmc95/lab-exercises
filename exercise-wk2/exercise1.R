9832ec5898a2f710e18f43ac4a93867cc64207f2
Aidan Muller-Cohn <aidanmc95@gmail.com>
2018-01-16 17:29:02 -0800

    Did some of the things asked

--- exercise-wk2/exercise1.R
+++ exercise-wk2/exercise1.R
@@ -20,19 +20,42 @@ print(str_length(my.line))
 # Describe why this doesn't work: 
 # 
 
+## Part 1: Debugging
+
+my.num <- 6
+initials <- "?. ?."
+
+my.vector <- c(my.num, initials)
+
+# run ?sum to get more info
+vector.sum <- sum(my.vector)
+
+# Describe why this doesn't work: 
+# These two values re different types
+
+install.packages("stringr")
+
+my.line <- "Hey, hey, this is the library"
+
+print(str_length(my.line))
+
+# Describe why this doesn't work: 
+# str_length is not defined correctly
+
 said.the.famous <- paste(my.line, " - ", initial)
 
 # Describe why this doesn't work: 
-# 
+# Wrong variable name
 
 
 ## Part 2 - Vector and function practice
 
 # Make a vector and use typeof to check what type R considers it to be
-
+my.vector2 <- c(1, 2, 3, 4, 5)
 # Write a function `CompareLength` that takes in 2 vectors, and returns the sentence:
 # "The difference in lengths is N"
-
+my.difference <- length(my.vector) - length(my.vector2)
+print("The difference in lengths is ", my.difference)
 
 # Pass two vectors of different length to your `CompareLength` function
 
@@ -49,5 +72,3 @@ said.the.famous <- paste(my.line, " - ", initial)
 # Send 3 vectors to your function to test it.
 
 # Write a function `CapsTime` that takes in a vector of names of courses you're taking, ex "Informatics 201" and removes all capital letters
-
-
